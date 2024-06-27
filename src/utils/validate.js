be9087bfd672477ebf315eafdc0c5eb05a02336a
AnandAnkur404 <130168218+AnandAnkur404@users.noreply.github.com>
2024-06-27 15:17:46 +0530

    form validation changes

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -8,7 +8,6 @@ export const checkValidData = (email, password) => {
   const isValidPassword =
     /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password);
 
-  // validation is not working, getting regex error on email and password
 
   if (!isEmailValid) return "Email is not valid";
   if (!isValidPassword) return "Password is not valid";
